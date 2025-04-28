dfb86ac6ac6e2e7f837a4f2fadd78a3920cb6ca1
Tassi <tassi.tbj@gmail.com>
2025-04-28 11:01:43 -0300

    Creacion de forms.py , para luego crear o editar comentarios.

---
+++ app/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from .models import Comment
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['title', 'text']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'text': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
+        }

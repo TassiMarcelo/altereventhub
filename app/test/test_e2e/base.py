69f64b1f01740154ab85724093f279348b24fd9c
DRVR1 <ianvidmar200@gmail.com>
2025-05-26 21:28:50 -0300

    base para e2e

--- app/test/test_e2e/base.py
+++ app/test/test_e2e/base.py
@@ -4,6 +4,7 @@ from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from playwright.sync_api import sync_playwright
 
 from app.models import User
+from app.models import *
 
 os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
 headless = os.environ.get("HEADLESS", 1) == 1
@@ -34,15 +35,45 @@ class BaseE2ETest(StaticLiveServerTestCase):
         # Cerrar la página después de cada test
         self.page.close()
 
-    def create_test_user(self, is_organizer=False):
+    def create_test_user(self,username="usuario_test",password="usuario_test",email="test@example.com", is_organizer=False):
         """Crea un usuario de prueba en la base de datos"""
-        return User.objects.create_user(
-            username="usuario_test",
-            email="test@example.com",
-            password="password123",
+        user =  User.objects.create_user(
+            username=username,
+            email=email,
+            password=password,
             is_organizer=is_organizer,
         )
+        user.save()
+        return user
+    
 
+    def create_test_category(self):
+        return Category.objects.create(
+            name="Música",
+            description="Eventos relacionados con conciertos y festivales.",
+            is_active=True
+        )
+    
+    def create_test_venue(self,capacity:int):
+        return Venue.objects.create(
+            name="Estadio Central",
+            address="Av. Siempre Viva 123",
+            city="Springfield",
+            capacity=capacity,
+            contact="contacto@estadiocentral.com"
+        )
+    
+    def create_test_event(self,organizer,venue):
+        return Event.objects.create(
+        title="Festival de Jazz",
+        description="Un evento musical imperdible.",
+        scheduled_at=timezone.now() + timezone.timedelta(days=30),
+        organizer=organizer,
+        venue=venue,
+        status=Event.Status.ACTIVO
+    )
+
+    
     def login_user(self, username, password):
         """Método auxiliar para iniciar sesión"""
         self.page.goto(f"{self.live_server_url}/accounts/login/")

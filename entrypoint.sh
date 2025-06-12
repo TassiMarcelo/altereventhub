0e590deda8e696f421341d2b5a134cc8df220ac4
cari395 <carinimatu11@live.com.ar>
2025-06-11 22:58:46 -0300

    Cambios al cd

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 
-# Aplicar migraciones antes de arrancar el servidor
+# Verificar configuración
+echo "Usando DJANGO_SETTINGS_MODULE: $DJANGO_SETTINGS_MODULE"
+
+# Aplicar migraciones
 echo "Aplicando migraciones..."
 python manage.py migrate --noinput
 
-# Ejecutar el comando original (Gunicorn)
+
+if [ "$DJANGO_ENV" = "production" ] || [ "$DJANGO_SETTINGS_MODULE" = "eventhub.settings" ]; then
+    echo "Colectando archivos estáticos..."
+    python manage.py collectstatic --noinput
+fi
+
+# Ejecutar el comando principal
 exec "$@"

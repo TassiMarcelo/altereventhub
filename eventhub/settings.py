f3a56d3375560c8ad693305380c0aa59dba7c842
Tassi <tassi.tbj@gmail.com>
2025-06-11 21:55:02 -0300

    cambio el cd para probar

--- eventhub/settings.py
+++ eventhub/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "localhost").split(",")
 
 
 # Application definition
